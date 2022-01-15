31b27d8273b074fd4a4776c8fbe91e411182bf77
brunosimplicio85 <57268760+brunosimplicio85@users.noreply.github.com>
2022-01-15 09:27:53 -0300

    Update Config.java

--- Finaceiro/src/java/util/Config.java
+++ Finaceiro/src/java/util/Config.java
@@ -7,10 +7,10 @@ public String whatsapp = "(31)97527-5084";
 public String telfixo = "(31)3333-3333";
 public String whatsappLink = "5531975275084";
 public int itensPaginacao = 3;
-public String nomeDaImobiliaria = "Imobiliária IMOB";
+public String nomeDaImobiliaria = "Financeiro";
 
 //CONFIGURAÇÕES DO EMAIL
-public String email = "projetoimobiliariahv@gmail.com";
+public String email = "projetfin@gmail.com";
 public String senhaEmail = "teste@1234";
 //COLOQUE ABAIXO gmail ou hotmal (De acordo com seu email para as portas do STTP
 public String tipoEmail = "gmail";
